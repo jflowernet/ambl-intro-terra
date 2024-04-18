1ac1a50113dea4957d90ea877c86fe86993de757
Jason Flower <jflowernet@aim.com>
2024-04-18 11:55:43 +1000

    reset

---
+++ data/data_prep.R
@@ -0,0 +1,72 @@
+#preparation of data for the intro to rasters in terra workshop
+
+library(terra)
+
+#extent of GBR found from the Great Barrier Reef Marine Park boundary, downloaded from: https://geohub-gbrmpa.hub.arcgis.com/datasets/bac38dff14ae4ff9a1c9de5d234e26f8_30/explore
+
+gbr_boundary <- vect("data/Great_Barrier_Reef_Marine_Park_Boundary_94_-8322827438515261938.gpkg")
+ext(gbr_boundary)
+
+#sea surface temperature data for an area covering the Great Barrier Reef retrieved from the NOAA ERDDAP server: https://coastwatch.pfeg.noaa.gov/erddap/griddap/NOAA_DHW.html
+#Daily data retrieved for 2020 - 2023: there was a mass coral bleaching event in 2022 so expect to see that in the temperature signature
+#Downloaded as 3 year time slices as trying to download the whole lot as one wasn't working
+
+## File Download - run once, then saved
+tmp <- tempfile()
+options(timeout = 6000)
+
+#this is a 219 MB download
+download.file("https://coastwatch.pfeg.noaa.gov/erddap/griddap/NOAA_DHW.nc?CRW_SST%5B(2020-05-31T12:00:00Z):1:(2021-05-31T12:00:00Z)%5D%5B(-10):1:(-25)%5D%5B(142):1:(155)%5D", tmp)
+
+gbr_area_sst <- rast(tmp)
+
+#data is inverted, so need to flip
+gbr_area_sst_corrected <- flip(gbr_area_sst)
+
+#this is a 219 MB download
+download.file("https://coastwatch.pfeg.noaa.gov/erddap/griddap/NOAA_DHW.nc?CRW_SST%5B(2021-06-01T12:00:00Z):1:(2022-05-31T12:00:00Z)%5D%5B(-10):1:(-25)%5D%5B(142):1:(155)%5D", tmp)
+
+gbr_area_sst2 <- rast(tmp)
+
+gbr_area_sst_corrected2 <- flip(gbr_area_sst2)
+
+#this is a 219 MB download
+download.file("https://coastwatch.pfeg.noaa.gov/erddap/griddap/NOAA_DHW.nc?CRW_SST%5B(2022-06-01T12:00:00Z):1:(2023-05-31T12:00:00Z)%5D%5B(-10):1:(-25)%5D%5B(142):1:(155)%5D", tmp)
+
+gbr_area_sst3 <- rast(tmp)
+
+gbr_area_sst_corrected3 <- flip(gbr_area_sst3)
+
+gbr_area_sst_stack <- c(gbr_area_sst_corrected, gbr_area_sst_corrected2, gbr_area_sst_corrected3)
+
+plot(gbr_area_sst_stack, fun = function()lines(gbr_boundary))
+#looks good!
+
+#save as cdf
+writeCDF(gbr_area_sst_stack, "data/gbr_area_sst_2020_2023.nc", overwrite = TRUE)
+
+## END download script
+
+#read above file back in for quicker data prep operations down the track
+gbr_daily_temp <- rast("data/gbr_area_sst_2020_2023.nc") |>
+  subset(1, negate = TRUE) #drop the first raster - accidentally included 31 May 2020 (i.e. only 1 day - so don't want to include when averaging by month)
+
+#save a daily tiff
+names(gbr_daily_temp) <- time(gbr_daily_temp) |>
+  format("%Y_%m_%d")
+
+time(gbr_daily_temp) <- NULL
+writeRaster(gbr_daily_temp, "data/gbr_daily_temp.tif", overwrite = T)
+
+#create monthly mean temp geotiff
+gbr_monthly_temp <- tapp(gbr_daily_temp, index = "yearmonths", fun = mean)
+
+writeRaster(gbr_monthly_temp, "data/gbr_monthly_temp.tif")
+
+mean_monthly_temp <- global(gbr_monthly_temp, mean, na.rm = TRUE)
+
+mean_monthly_temp$yearmonth <- rownames(mean_monthly_temp)
+
+plot(mean_monthly_temp$mean, type = "b")
+
+#save
